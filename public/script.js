f887c8dc8b4df85c1480d3a9859971ce6bd4f3ee
Hugo Jordao <hugotjordao@hotmail.com>
2021-12-29 00:20:30 +0000

    initial commit

---
+++ public/script.js
@@ -0,0 +1,128 @@
+//to run port 3001 do Set-ExecutionPolicy RemoteSigned on windows powershell
+//when done do Set-ExecutionPolicy Restricted
+
+const socket = io('/') //socket connects to root path
+const videoGrid = document.getElementById('video-grid') //video ref
+
+//create peer server connection
+const myPeer = new Peer(undefined, {
+    host: '/',
+    port: '3001'
+})
+const myVideo = document.createElement('video')
+myVideo.muted = true //muting our audio to ourselves
+
+const peers = {}
+const peers_vids = {}
+var user
+
+navigator.mediaDevices.getUserMedia({
+    video: true, //passing our video and audio to peers
+    audio: true
+}).then(stream => {
+    addVideoStream(myVideo, stream);
+
+    myPeer.on('call', call => {
+        call.answer(stream)
+
+        var video = document.createElement('video')
+        call.on('stream', userVideoStream => {
+            addVideoStream(video, userVideoStream)
+        })
+
+        peers[1] = call
+        peers_vids[1] = video
+        
+    })
+
+    socket.on('user-connected', userId => {
+        setTimeout(connectToNewUser,1000,userId,stream)
+        //connectToNewUser(userId, stream);
+    })
+})
+
+
+socket.on('user-disconnected', userId => {
+    if(peers[userId]) peers[userId].close();
+    if(peers[1]){
+        peers_vids[1].remove();
+        peers[1].close();
+    }
+})
+
+//as soon as we connect with our peer this runs
+myPeer.on('open', id => {
+    user = id;
+    socket.emit('join-room', ROOM_ID, id);
+})
+ 
+
+function connectToNewUser(userId, stream) {
+    const call = myPeer.call(userId, stream)
+    console.log("wtf")
+    const video = document.createElement('video')
+    call.on('stream', userVideoStream => { //when new user, add their video
+        addVideoStream(video, userVideoStream)
+
+    })
+    call.on('close', () => { //if user leaves, close video
+        video.remove()
+    })
+
+    peers[userId] = call
+    peers_vids[userId] = video
+}
+
+//associating video object with a stream
+function addVideoStream(video, stream){
+    video.srcObject = stream
+    video.addEventListener('loadedmetadata', () => {
+        video.play()
+    })
+    videoGrid.append(video)
+}
+
+
+//pausing the video stream
+function pauseVideo(){
+    myVideo.pause()
+    socket.emit('user-paused', user)
+
+    const button = document.getElementById('pause')
+    button.style.display = 'none'
+
+    const button2 = document.getElementById('unpause')
+    button2.style.display = 'inline-block'
+}
+
+//pausing the peers video stream if it clicked on pause
+socket.on('peer-paused', user => {
+    if(peers_vids[user]){
+        peers_vids[user].pause()
+    }
+    if(peers[1]){
+        peers_vids[1].pause();
+    }
+})
+
+//unpausing video stream
+function unpauseVideo(){
+    myVideo.play()
+    socket.emit('user-unpaused', user)
+
+    const button = document.getElementById('unpause')
+    button.style.display = 'none'
+
+    const button2 = document.getElementById('pause')
+    button2.style.display = 'inline-block'
+}
+
+//unpausing the peers video stream if it clicked on pause
+socket.on('peer-unpaused', user => {
+    if(peers_vids[user]){
+        peers_vids[user].play()
+    }
+    if(peers[1]){
+        peers_vids[1].play();
+    }
+})
---
+++ server.js
@@ -0,0 +1,48 @@
+//create a server to be used with socket.io
+const express = require('express'); //create express server
+const app = express(); //to run server
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const { v4: uuidV4 } = require('uuid')
+
+
+//setup the express server
+app.set('view engine', 'ejs');
+app.use(express.static('public')); //all js and css goes in this public folder
+
+//redirect user to a room
+app.get('/', (req, res)  => {
+    res.redirect(`/${uuidV4()}`)
+})
+
+//create a brand new room for user
+app.get('/:room', (req, res)  => {
+    res.render('room', { roomId: req.params.room })
+})
+
+//to run every time someone connects to the webpage
+io.on('connection', socket => {
+    socket.on('join-room', (roomId, userId) => {
+        console.log(roomId, userId)
+
+        socket.join(roomId)
+        socket.to(roomId).emit("user-connected", userId)
+
+
+        socket.on('user-paused', user => {
+            socket.to(roomId).emit('peer-paused', user)
+        })
+
+        socket.on('user-unpaused', user => {
+            socket.to(roomId).emit('peer-unpaused', user)
+        })
+
+        socket.on('disconnect', () => {
+            socket.to(roomId).emit('user-disconnected', userId)
+        })
+    })
+})
+
+//make server in port 3000
+server.listen(3000);
+
