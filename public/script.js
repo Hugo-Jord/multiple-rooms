7621a5f32b4b4a0848d825ff272215a0177b8923
Hugo Jordao <hugotjordao@hotmail.com>
2022-01-01 22:45:55 +0000

    I HATE THIS 3

--- public/script.js
+++ public/script.js
@@ -30,11 +30,6 @@ navigator.mediaDevices.getUserMedia({
         var video = document.createElement('video')
         video.style.width = '200px'
         video.style.height = '200px'
-        video.style.marginLeft = '100px'
-        video.style.marginTop = '10px'
-        video.style.left = '52%'
-        video.style.top = '50%'
-        video.style.position = 'absolute'
         call.on('stream', userVideoStream => {
             addVideoStream(video, userVideoStream)
         })
@@ -72,11 +67,6 @@ function connectToNewUser(userId, stream) {
     const video = document.createElement('video')
     video.style.width = '200px'
     video.style.height = '200px'
-    video.style.marginLeft = '100px'
-    video.style.marginTop = '10px'
-    video.style.left = '52%'
-    video.style.left = '50%'
-    video.style.position = 'absolute'
     call.on('stream', userVideoStream => { //when new user, add their video
         addVideoStream(video, userVideoStream)
 
