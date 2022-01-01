b83ba09cb49a677f6bb4bf048b92dda96c08249e
Hugo Jordao <hugotjordao@hotmail.com>
2022-01-01 23:14:27 +0000

    buttoow2ewnsad

--- public/script.js
+++ public/script.js
@@ -30,7 +30,7 @@ navigator.mediaDevices.getUserMedia({
         var video = document.createElement('video')
         video.style.width = '200px'
         video.style.height = '200px'
-        video.style.left = '1%'
+        video.style.left = '10%'
         call.on('stream', userVideoStream => {
             addVideoStream(video, userVideoStream)
         })
@@ -68,7 +68,7 @@ function connectToNewUser(userId, stream) {
     const video = document.createElement('video')
     video.style.width = '200px'
     video.style.height = '200px'
-    video.style.left = '1%'
+    video.style.left = '10%'
     call.on('stream', userVideoStream => { //when new user, add their video
         addVideoStream(video, userVideoStream)
 
