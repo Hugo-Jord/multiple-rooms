eb3003c1709c7bcd85a3222fd1606318f9f074a9
Hugo Jordao <hugotjordao@hotmail.com>
2022-01-05 15:29:03 +0000

    firebase init

---
+++ public/firebase/index.js
@@ -0,0 +1,16 @@
+//Firebase configuration
+import firebase from "firebase/app"
+import "firebase/storage"
+const firebaseConfig = {
+    apiKey: "AIzaSyAMNRdeSeupmt3q-pfRJ6slzQZsMDwwKT0",
+    authDomain: "passenger-95dd3.firebaseapp.com",
+    databaseURL: "https://passenger-95dd3-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "passenger-95dd3",
+    storageBucket: "passenger-95dd3.appspot.com",
+    messagingSenderId: "263648786960",
+    appId: "1:263648786960:web:4d39c863d96a5b3974e895"
+  };
+
+ firebase.initializeApp(firebaseConfig)
+ const storage = firebase.storage()
+ export { storage, firebase as default}
--- public/script.js
+++ public/script.js
@@ -1,19 +1,3 @@
-//Firebase configuration
-import firebase from "firebase/app"
-
-const firebaseConfig = {
-    apiKey: "AIzaSyAMNRdeSeupmt3q-pfRJ6slzQZsMDwwKT0",
-    authDomain: "passenger-95dd3.firebaseapp.com",
-    databaseURL: "https://passenger-95dd3-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "passenger-95dd3",
-    storageBucket: "passenger-95dd3.appspot.com",
-    messagingSenderId: "263648786960",
-    appId: "1:263648786960:web:4d39c863d96a5b3974e895"
-  };
-
- firebase.initializeApp(firebaseConfig)
- const storage = firebase.storage()
-
 //to run port 3001 do Set-ExecutionPolicy RemoteSigned on windows powershell
 //when done do Set-ExecutionPolicy Restricted
 const socket = io('/') //socket connects to root path
