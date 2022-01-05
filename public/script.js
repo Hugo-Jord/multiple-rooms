eb5bee564b191616aa3ccaddc04ff67b65056291
Hugo Jordao <hugotjordao@hotmail.com>
2022-01-05 15:16:09 +0000

    firebase init

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
 //Firebase configuration
 import firebase from "firebase/app"
-import "firebase/storage"
+require("firebase/storage")
 const firebaseConfig = {
     apiKey: "AIzaSyAMNRdeSeupmt3q-pfRJ6slzQZsMDwwKT0",
     authDomain: "passenger-95dd3.firebaseapp.com",
